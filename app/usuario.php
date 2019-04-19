bafb9b86d77566c85e90aa518b8f9a47cc46b196
valeriacy <valecyuricev@gmail.com>
2019-04-19 19:05:25 -0400

    modelo para usuario

--- app/usuario.php
+++ app/usuario.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class usuario extends Model
 {
-    protected $fillable = ['nombre','contra'];
+    protected $fillable = ['nombre', 'apellido', 'rol', 'nombre_usuario', 'contra'];
 }
