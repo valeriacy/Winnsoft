ff53bf2b9b908a06765a96a756ccb5ec2cf04bf6
valeriacy <valecyuricev@gmail.com>
2019-04-19 19:01:50 -0400

    Creada nueva migracion usuario

--- database/migrations/2019_04_19_224046_create_usuarios_table.php
+++ database/migrations/2019_04_19_224046_create_usuarios_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Primera extends Migration
+class CreateUsuariosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -16,8 +16,11 @@ class Primera extends Migration
         Schema::create('usuarios', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nombre');
+            $table->string('apellido');
+            $table->string('rol');
+            $table->string('nombre_usuario');
             $table->string('contra');
-            
+            $table->timestamps();
         });
     }
 
