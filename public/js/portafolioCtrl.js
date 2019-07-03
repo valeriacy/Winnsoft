33df7a2b43784bb5b26a377ca56dc9596e51c0b8
JpRod96 <jprodriguez60@gmail.com>
2019-07-03 15:59:25 -0400

    Creado nuevo controlador portafolioCtrl

---
+++ public/js/portafolioCtrl.js
@@ -0,0 +1,186 @@
+class PortafolioCtrl{
+    constructor(httpService, scope) {
+        this.httpService = httpService;
+        this.scope = scope;
+        this.obtenerSesionesPorGrupo=this.obtenerSesionesPorGrupo.bind(this);
+        this.filtrarEntregaUsuario=this.filtrarEntregaUsuario.bind(this);
+        this.filtrarEntregaUsuarioEnSesion=this.filtrarEntregaUsuarioEnSesion.bind(this);
+        this.guardarEntrega=this.guardarEntrega.bind(this);
+        this.guardarProducto=this.guardarProducto.bind(this);
+        this.crearEntrega=this.crearEntrega.bind(this);
+      }
+    
+    obtenerSesionesPorGrupo(idOferta, idUsuario){
+      let req={
+          method: 'GET',
+          url: "/api/sesiones/"+idOferta,
+      };
+  
+      this.httpService(req)
+      .then((response)=>{
+          let data=response.data;
+          this.filtrarEntregaUsuario(data, idUsuario);
+          this.scope.sesiones = data;
+          
+      })
+      .catch((error)=>{
+          console.error(error);
+      });
+    }
+
+  filtrarEntregaUsuario(sesiones, id){
+    if(sesiones)
+        sesiones.reverse();
+    for(let sesion of sesiones){
+        this.filtrarEntregaUsuarioEnSesion(sesion, id);
+    }
+  }
+  
+  filtrarEntregaUsuarioEnSesion(sesion, id){
+    let productos = sesion.productos;
+    if(productos)
+        productos.reverse()
+    for(let producto of productos){
+        let entregasGlobales=producto.entregas;
+        let entregasUsuario = entregasGlobales.filter(entrega => entrega.usuario_id==id);
+        producto.entregas=entregasUsuario;
+    }
+  }
+
+  guardarProducto(producto, usuarioId, grupoId){
+    let req = {
+        method: 'POST',
+        url: "/api/Producto",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data : JSON.stringify(producto)
+    }
+    consumirApi(this.httpService,
+                    req, 
+                    (response)=>{
+                        alert("Creado nuevo producto");
+                        ocultarFormProducto(producto.sesionId);
+                        this.obtenerSesionesPorGrupo(grupoId, usuarioId);
+                    },
+                    (error)=>{
+                        console.error(error);
+                    }
+                )
+  }
+
+  guardarEntrega(productoId){
+    
+    let boton = document.querySelector("#enviar-"+productoId);
+    let gif = document.querySelector("#loading-"+productoId);
+
+    boton.style.display="none";
+    gif.style.display="block";
+
+    let textArea=document.querySelector("#descripcion-"+productoId);
+    let fileInput=document.querySelector("#file-"+productoId);
+    if(validarCamposEntrega(productoId)){
+        if(validarArchivoEstudiante(fileInput)){
+            let text=textArea.value;
+            let withFile=fileInput.value !== "";
+
+            let entrega = {
+                descripcion:text,
+                usuarioId:usuario.id,
+                productoId:productoId
+            }
+            this.crearEntrega(entrega, withFile, productoId);
+        }else{
+            let boton = document.querySelector("#enviar-"+productoId);
+            let gif = document.querySelector("#loading-"+productoId);
+
+            boton.style.display="block";
+            gif.style.display="none";
+        }
+    }else{
+        alert("Al menos uno de los campos debe ser llenado");
+        let boton = document.querySelector("#enviar-"+productoId);
+        let gif = document.querySelector("#loading-"+productoId);
+
+        boton.style.display="block";
+        gif.style.display="none";
+    }
+  }
+
+  crearEntrega(entrega, withFile, productoId){
+    let req = {
+        method: 'POST',
+        url: "/api/Entrega",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data : JSON.stringify(entrega)
+    }
+    consumirApi(this.httpService,
+                    req, 
+                    (response)=>{
+                        let entregaId = response.data;
+                        if(withFile)
+                            this.uploadFile(entregaId, productoId, false);
+                        else
+                            this.reemplazarDivEntrega(entregaId, this.httpService, this.scope, productoId);
+                    },
+                    (error)=>{
+                        console.error(error);
+                    }
+                )
+  }
+
+  uploadFile (entregaId, productoId, deDocente){
+    let url = deDocente ? '/api/subirArchivoDocente/'+entregaId : '/api/subirArchivo/'+entregaId
+    let request = {
+        method: 'POST',
+        url: url,
+        data: formData,
+        headers: {
+            'Content-Type': undefined
+        }
+    };
+    consumirApi(this.httpService,
+                request,
+                (response)=>{
+                    if(!deDocente)
+                        this.reemplazarDivEntrega(entregaId, this.httpService, this.scope, productoId);
+                    else
+                        this.scope.restablecer();
+                },
+                (error)=>{console.error(error)})
+}
+
+reemplazarDivEntrega(entregaId, productoId){
+    consumirApi(this.httpService,
+        {
+            method: 'GET',
+            url: '/api/entrega/'+entregaId,
+        }, 
+        (response)=>{
+            let entrega = response.data;
+            this.agregarAProducto(entrega, productoId);
+        },
+        (error)=>{
+            console.error(error);
+        })
+    alert("Entrega exitosa");
+}
+
+agregarAProducto(entrega, productoId){
+    let productoPadre = entrega.producto_id;
+    for(let sesion of this.scope.sesiones){
+        for(let producto of sesion.productos){
+            if(producto.id === productoPadre)
+                producto.entregas.push(entrega);
+                let boton = document.querySelector("#enviar-"+productoId);
+                let gif = document.querySelector("#loading-"+productoId);
+    
+                boton.style.display="block";
+                gif.style.display="none";
+        }
+    }
+}
+
+}
